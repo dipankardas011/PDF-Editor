b1e616a35430391095dbc22e065da3231afc9770
Dipankar Das <dipsonu10@hotmail.com>
2022-07-15 14:32:05 +0530

    Added tekton pipeline

---
+++ src/frontend/__tests__/simple.js
@@ -0,0 +1,23 @@
+
+const server = require('../server')
+const supertest = require('supertest');
+const requestWithSupertest = supertest(server);
+
+describe("Testing with Jest", () => {
+  test("Addition", () => {
+    const sum = 2 + 3;
+    const expectedResult = 5;
+    expect(sum).toEqual(expectedResult);
+  });
+});
+
+describe('User Endpoints', () => {
+
+  it('GET / homepage', async () => {
+    const res = await requestWithSupertest.get('/');
+      expect(res.status).toEqual(200);
+      expect(res.type).toEqual(expect.stringContaining('html'));
+      // expect(res.body).toHaveProperty('users')
+  });
+
+});
