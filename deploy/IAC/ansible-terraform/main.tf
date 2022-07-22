78ef4a8996a9b027f17af51e163595620fd93e04
Dipankar Das <dipsonu10@hotmail.com>
2022-07-22 17:53:04 +0530

    Tf aws

--- deploy/IAC/ansible-terraform/main.tf
+++ deploy/IAC/ansible-terraform/main.tf
@@ -1,3 +1,14 @@
+backend "remote" {
+  # The name of your Terraform Cloud organization.
+  organization = "pdf-org"
+  # The name of the Terraform Cloud workspace to store Terraform state files in.
+  workspaces {
+     name = "PDF-Editor"
+  }
+}
+
+
+
 provider "aws" {
   region  = "us-east-1"
 }
