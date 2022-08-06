87290de3378ff98d727e32a69c7dc76d2e12e2b8
Dipankar Das <dipsonu10@hotmail.com>
2022-08-06 18:38:15 +0530

    [Test](Integration) Added basic setup
    Signed-off-by: Dipankar Das <dipsonu10@hotmail.com>

---
+++ test/integration/test-merger.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# create container backend
+cd ../../src/backend/merger
+docker build --target prod -t backend .
+
+# create container frontend
+cd ../../frontend
+docker build --target prod -t frontend .
+
+# create a isolated network
+docker network create xyz
+
+docker run --rm \
+  -d \
+  --net xyz \
+  --name backend-merge \
+  -p 8080 \
+  backend
+
+docker run --rm \
+  -d \
+  --net xyz \
+  --name frontend \
+  -p 80:80 \
+  frontend
+
+sleep 5
+
+# test the curl
+curl -X GET http://localhost/
+
+echo "Clean up"
+
+docker rm -f frontend backend-merge
+docker network rm xyz
