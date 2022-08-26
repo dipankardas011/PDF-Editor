1d195fe27f70ec66b65a526729545842cc19b2a2
Dipankar Das <dipsonu10@hotmail.com>
2022-08-26 08:18:22 +0530

    [Test, Okteto](Integration)Added Integration test and okteto for rotator

--- test/integration/test-merger.sh
+++ test/integration/test-merger.sh
@@ -2,13 +2,17 @@
 
 # All the global variables
 counterEndpoints=0
-NO_OF_ENDPOINTS=6
+NO_OF_ENDPOINTS=8
 PORT=9090
 isUploadSuccess=1
 
-# create container backend
+# create container merger
 cd ../../src/backend/merger
 docker build --target prod -t backend .
+
+# create container rotator
+cd ../rotator
+docker build --target prod -t rotate .
 
 # create container frontend
 cd ../../frontend
@@ -33,6 +37,13 @@ docker run --rm \
 docker run --rm \
   -d \
   --net xyz \
+  --name backend-rotate \
+  -p 8081 \
+  rotate
+
+docker run --rm \
+  -d \
+  --net xyz \
   --name frontend \
   -p $PORT:80 \
   frontend
@@ -42,7 +53,8 @@ cd ../../test/integration
 Call_Cleanup() {
   echo -e "\n================================================================"
   echo -e "\n$(tput setaf 3)$(tput bold)Cleanup Started$(tput init)"
-  docker rm -f frontend backend-merge
+  docker rm -f frontend backend-merge backend-rotate
+  docker rmi -f rotate frontend backend
   cd ../../src/frontend/
   # making the upload button act like both upload and download
   sed -i "s/res.send(ccc2)/(isSuccessfull) ? res.redirect('\/merge\/download') : res.send(storeError)/g" server.js
@@ -50,6 +62,7 @@ Call_Cleanup() {
   cd -
   docker network rm xyz
   rm -f merged.pdf
+  rm -f rotated.pdf
   echo -e "$(tput setaf 2)$(tput bold) ✓ [DONE] Cleanup$(tput init)"
 }
 
@@ -60,124 +73,130 @@ echo -e "\n$(tput setaf 3)$(tput bold)- /$(tput init)"
 echo -e "$(tput setaf 3)$(tput bold)- /about$(tput init)"
 echo -e "$(tput setaf 3)$(tput bold)- /merger$(tput init)"
 echo -e "$(tput setaf 3)$(tput bold)- /rotator$(tput init)"
-echo -e "$(tput setaf 3)$(tput bold)- /merge/upload$(tput init)"
+echo -e "\n$(tput setaf 3)$(tput bold)- /merge/upload$(tput init)"
 echo -e "$(tput setaf 3)$(tput bold)- /merge/download$(tput init)"
+echo -e "\n$(tput setaf 3)$(tput bold)- /rotate/upload$(tput init)"
+echo -e "$(tput setaf 3)$(tput bold)- /rotate/download$(tput init)"
 
+AllFrontendEndpoint() {
 
-# @@ ALL GENERAL INDEX FILE TESTING WITH THEIR ENDPOINTS
+  # @@ ALL GENERAL INDEX FILE TESTING WITH THEIR ENDPOINTS
 
-echo -e "\n$(tput setaf 5)$(tput bold)Testing Endpoint /$(tput init)"
-hello=$(curl -X GET http://localhost:$PORT | grep -e '<a class="navbar.*" href="#">PDF Editor.*<\/a>' | wc -l)
+  echo -e "\n$(tput setaf 5)$(tput bold)Testing Endpoint /$(tput init)"
+  hello=$(curl -X GET http://localhost:$PORT | grep -e '<a class="navbar.*" href="#">PDF Editor.*<\/a>' | wc -l)
 
-if [[ $hello -eq 0 ]]
-then
-  echo -e "\n$(tput setaf 1)$(tput bold)✗ [Failed] the test of Endpoint /$(tput init)"
-else
-  counterEndpoints=$((counterEndpoints+1))
-  echo -e "\n$(tput setaf 2)$(tput bold)✓ [Passed] the test of Endpoint /$(tput init)"
-fi
-
-echo "----------------------------------------------------------------"
-
-
-echo -e "\n$(tput setaf 5)$(tput bold)Testing Endpoint /about$(tput init)"
-hello=$(curl -X GET http://localhost:$PORT/about | grep -e '<a class="navbar.*" href="#">PDF Editor.*<\/a>' | wc -l)
-
-if [[ $hello -eq 0 ]]
-then
-  echo -e "\n$(tput setaf 1)$(tput bold)✗ [Failed] the test of Endpoint /about$(tput init)"
-else
-  counterEndpoints=$((counterEndpoints+1))
-  echo -e "\n$(tput setaf 2)$(tput bold)✓ [Passed] the test of Endpoint /about$(tput init)"
-fi
-
-
-echo "----------------------------------------------------------------"
-
-echo -e "\n$(tput setaf 5)$(tput bold)Testing Endpoint /merger$(tput init)"
-
-hello=$(curl -X GET http://localhost:$PORT/merger | grep -e '<a class="navbar.*" href="#">PDF Merge.*<\/a>' | wc -l)
-
-if [[ $hello -eq 0 ]]
-then
-  echo -e "\n$(tput setaf 1)$(tput bold)✗ [Failed] the test of Endpoint /merger$(tput init)"
-else
-  counterEndpoints=$((counterEndpoints+1))
-  echo -e "\n$(tput setaf 2)$(tput bold)✓ [Passed] the test of Endpoint /merger$(tput init)"
-fi
-
-
-echo "----------------------------------------------------------------"
-
-echo -e "\n$(tput setaf 5)$(tput bold)Testing Endpoint /rotator$(tput init)"
-
-hello=$(curl -X GET http://localhost:$PORT/rotator | grep -e '<a class="navbar.*" href="#">PDF Rotate.*<\/a>' | wc -l)
-
-if [[ $hello -eq 0 ]]
-then
-  echo -e "\n$(tput setaf 1)$(tput bold)✗ [Failed] the test of Endpoint /rotator$(tput init)"
-else
-  counterEndpoints=$((counterEndpoints+1))
-  echo -e "\n$(tput setaf 2)$(tput bold)✓ [Passed] the test of Endpoint /rotator$(tput init)"
-fi
-
-
-# @@ TESTING ALL MERGER ENDPOINTS
-
-echo "----------------------------------------------------------------"
-
-echo -e "\n$(tput setaf 5)$(tput bold)Testing Endpoint '/merge/upload'$(tput init)"
-flag=0
-echo -e "\n$(tput setaf 6)$(tput bold)  ↕ Upload file 01.pdf$(tput init)"
-
-hello=$(curl --raw --form "myFile=@${PWD}/resources/01.pdf" --form "myFile=@${PWD}/resources/02.pdf" http://localhost:${PORT}/merge/upload | grep -e '<div.*;alert-success role="alert">Uploaded.*<\/div>' | wc -l)
-if [[ $hello -eq 0 ]]; then
-  echo -e "\n$(tput setaf 1)$(tput bold)  ↕ ✗ [Failed] to upload 01.pdf & 02.pdf$(tput init)"
-  flag=1
-else
-  echo -e "\n$(tput setaf 6)$(tput bold)  ↕ ✓ [Passed] to upload 01.pdf & 02.pdf$(tput init)"
-fi
-
-
-if [[ $flag -eq 1 ]]; then
-  echo -e "\n$(tput setaf 1)$(tput bold)✗ [Failed] the test of Endpoint /merge/upload$(tput init)"
-else
-  counterEndpoints=$((counterEndpoints+1))
-  isUploadSuccess=0
-  echo -e "\n$(tput setaf 2)$(tput bold)✓ [Passed] the test of Endpoint /merge/upload$(tput init)"
-fi
-
-echo "----------------------------------------------------------------"
-
-echo -e "\n$(tput setaf 5)$(tput bold)Testing Endpoint '/merge/upload & /merge/download'$(tput init)"
-
-if [[ $isUploadSuccess -eq 1 ]]; then
-  echo -e "\n$(tput setaf 1)$(tput bold)✗ [Failed] the test of Endpoint /merge/upload so [FAILED]$(tput init)"
-else
-  hello=$(curl --raw --output merged.pdf -X GET http://localhost:$PORT/merge/download && cat merged.pdf | head -n 5 | grep -be "%PDF.*" | wc -l) # return value is stored in hello
-  # hello=$(curl --raw --output merged.pdf --form "myFile=@${PWD}/resources/01.pdf" --form "myFile=@${PWD}/resources/02.pdf" -L http://localhost:$PORT/merge/upload && cat merged.pdf | head -n 5 | grep -be "%PDF.*" | wc -l) # return value is stored in hello
-  size01PDF=$(cat resources/01.pdf | tail -n 2 | head -n 1)
-  size02PDF=$(cat resources/02.pdf | tail -n 2 | head -n 1)
-  sizemergedPDF=$(cat merged.pdf | tail -n 2 | head -n 1)
-
-  # size of merged pdf should be larger than max between the 2 pdfs
-
-  if [[ $size01PDF -gt $size02PDF ]]; then
-    if [[ $sizemergedPDF -gt $size01PDF ]] && [[ $hello -gt 0 ]]; then
-      counterEndpoints=$((counterEndpoints+1))
-      echo -e "\n$(tput setaf 2)$(tput bold)✓ [Passed] the test of Endpoint /merge/download$(tput init)"
-    else
-      echo -e "\n$(tput setaf 1)$(tput bold)✗ [Failed] the test of Endpoint /merge/download$(tput init)"
-    fi
+  if [[ $hello -eq 0 ]]
+  then
+    echo -e "\n$(tput setaf 1)$(tput bold)✗ [Failed] the test of Endpoint /$(tput init)"
   else
-    if [[ $sizemergedPDF -gt $size02PDF ]] && [[ $hello -gt 0 ]]; then
-      counterEndpoints=$((counterEndpoints+1))
-      echo -e "\n$(tput setaf 2)$(tput bold)✓ [Passed] the test of Endpoint /merge/download$(tput init)"
+    counterEndpoints=$((counterEndpoints+1))
+    echo -e "\n$(tput setaf 2)$(tput bold)✓ [Passed] the test of Endpoint /$(tput init)"
+  fi
+
+  echo "----------------------------------------------------------------"
+
+
+  echo -e "\n$(tput setaf 5)$(tput bold)Testing Endpoint /about$(tput init)"
+  hello=$(curl -X GET http://localhost:$PORT/about | grep -e '<a class="navbar.*" href="#">PDF Editor.*<\/a>' | wc -l)
+
+  if [[ $hello -eq 0 ]]
+  then
+    echo -e "\n$(tput setaf 1)$(tput bold)✗ [Failed] the test of Endpoint /about$(tput init)"
+  else
+    counterEndpoints=$((counterEndpoints+1))
+    echo -e "\n$(tput setaf 2)$(tput bold)✓ [Passed] the test of Endpoint /about$(tput init)"
+  fi
+
+
+  echo "----------------------------------------------------------------"
+
+  echo -e "\n$(tput setaf 5)$(tput bold)Testing Endpoint /merger$(tput init)"
+
+  hello=$(curl -X GET http://localhost:$PORT/merger | grep -e '<a class="navbar.*" href="#">PDF Merge.*<\/a>' | wc -l)
+
+  if [[ $hello -eq 0 ]]
+  then
+    echo -e "\n$(tput setaf 1)$(tput bold)✗ [Failed] the test of Endpoint /merger$(tput init)"
+  else
+    counterEndpoints=$((counterEndpoints+1))
+    echo -e "\n$(tput setaf 2)$(tput bold)✓ [Passed] the test of Endpoint /merger$(tput init)"
+  fi
+
+
+  echo "----------------------------------------------------------------"
+
+  echo -e "\n$(tput setaf 5)$(tput bold)Testing Endpoint /rotator$(tput init)"
+
+  hello=$(curl -X GET http://localhost:$PORT/rotator | grep -e '<a class="navbar.*" href="#">PDF Rotate.*<\/a>' | wc -l)
+
+  if [[ $hello -eq 0 ]]
+  then
+    echo -e "\n$(tput setaf 1)$(tput bold)✗ [Failed] the test of Endpoint /rotator$(tput init)"
+  else
+    counterEndpoints=$((counterEndpoints+1))
+    echo -e "\n$(tput setaf 2)$(tput bold)✓ [Passed] the test of Endpoint /rotator$(tput init)"
+  fi
+}
+
+
+MergerTestCases() {
+
+  # @@ TESTING ALL MERGER ENDPOINTS
+
+  echo "----------------------------------------------------------------"
+
+  echo -e "\n$(tput setaf 5)$(tput bold)Testing Endpoint '/merge/upload'$(tput init)"
+  flag=0
+  echo -e "\n$(tput setaf 6)$(tput bold)  ↕ Upload file 01.pdf$(tput init)"
+
+  hello=$(curl --raw --form "myFile=@${PWD}/resources/01.pdf" --form "myFile=@${PWD}/resources/02.pdf" http://localhost:${PORT}/merge/upload | grep -e '<div.*;alert-success role="alert">Uploaded.*<\/div>' | wc -l)
+  if [[ $hello -eq 0 ]]; then
+    echo -e "\n$(tput setaf 1)$(tput bold)  ↕ ✗ [Failed] to upload 01.pdf & 02.pdf$(tput init)"
+    flag=1
+  else
+    echo -e "\n$(tput setaf 6)$(tput bold)  ↕ ✓ [Passed] to upload 01.pdf & 02.pdf$(tput init)"
+  fi
+
+
+  if [[ $flag -eq 1 ]]; then
+    echo -e "\n$(tput setaf 1)$(tput bold)✗ [Failed] the test of Endpoint /merge/upload$(tput init)"
+  else
+    counterEndpoints=$((counterEndpoints+1))
+    isUploadSuccess=0
+    echo -e "\n$(tput setaf 2)$(tput bold)✓ [Passed] the test of Endpoint /merge/upload$(tput init)"
+  fi
+
+  echo "----------------------------------------------------------------"
+
+  echo -e "\n$(tput setaf 5)$(tput bold)Testing Endpoint '/merge/upload & /merge/download'$(tput init)"
+
+  if [[ $isUploadSuccess -eq 1 ]]; then
+    echo -e "\n$(tput setaf 1)$(tput bold)✗ [Failed] the test of Endpoint /merge/upload so [FAILED]$(tput init)"
+  else
+    hello=$(curl --raw --output merged.pdf -X GET http://localhost:$PORT/merge/download && cat merged.pdf | head -n 5 | grep -be "%PDF.*" | wc -l) # return value is stored in hello
+    size01PDF=$(cat resources/01.pdf | tail -n 2 | head -n 1)
+    size02PDF=$(cat resources/02.pdf | tail -n 2 | head -n 1)
+    sizemergedPDF=$(cat merged.pdf | tail -n 2 | head -n 1)
+
+    # size of merged pdf should be larger than max between the 2 pdfs
+
+    if [[ $size01PDF -gt $size02PDF ]]; then
+      if [[ $sizemergedPDF -gt $size01PDF ]] && [[ $hello -gt 0 ]]; then
+        counterEndpoints=$((counterEndpoints+1))
+        echo -e "\n$(tput setaf 2)$(tput bold)✓ [Passed] the test of Endpoint /merge/download$(tput init)"
+      else
+        echo -e "\n$(tput setaf 1)$(tput bold)✗ [Failed] the test of Endpoint /merge/download$(tput init)"
+      fi
     else
-      echo -e "\n$(tput setaf 1)$(tput bold)✗ [Failed] the test of Endpoint /merge/download$(tput init)"
+      if [[ $sizemergedPDF -gt $size02PDF ]] && [[ $hello -gt 0 ]]; then
+        counterEndpoints=$((counterEndpoints+1))
+        echo -e "\n$(tput setaf 2)$(tput bold)✓ [Passed] the test of Endpoint /merge/download$(tput init)"
+      else
+        echo -e "\n$(tput setaf 1)$(tput bold)✗ [Failed] the test of Endpoint /merge/download$(tput init)"
+      fi
     fi
   fi
-fi
+}
 
 #
 #
@@ -185,8 +204,59 @@ fi
 #
 #
 
+RotatorTestCases() {
+  # @@ TESTING ALL Rotator ENDPOINTS
+
+  echo "----------------------------------------------------------------"
+
+  echo -e "\n$(tput setaf 5)$(tput bold)Testing Endpoint '/rotate/upload'$(tput init)"
+  flag=0
+  echo -e "\n$(tput setaf 6)$(tput bold)  ↕ Upload file 01.pdf$(tput init)"
+  # sleep 100
+  hello=$(curl --raw --form "myFile=@${PWD}/resources/01.pdf" --form "pages=," http://localhost:${PORT}/rotate/upload | grep -e '<div.*;alert-success role="alert">Uploaded.*<\/div>' | wc -l)
+  if [[ $hello -eq 0 ]]; then
+    echo -e "\n$(tput setaf 1)$(tput bold)  ↕ ✗ [Failed] to upload 01.pdf$(tput init)"
+    flag=1
+  else
+    echo -e "\n$(tput setaf 6)$(tput bold)  ↕ ✓ [Passed] to upload 01.pdf$(tput init)"
+  fi
+
+
+  if [[ $flag -eq 1 ]]; then
+    echo -e "\n$(tput setaf 1)$(tput bold)✗ [Failed] the test of Endpoint /rotate/upload$(tput init)"
+  else
+    counterEndpoints=$((counterEndpoints+1))
+    isUploadSuccess=0
+    echo -e "\n$(tput setaf 2)$(tput bold)✓ [Passed] the test of Endpoint /rotate/upload$(tput init)"
+  fi
+
+  echo "----------------------------------------------------------------"
+
+  echo -e "\n$(tput setaf 5)$(tput bold)Testing Endpoint '/rotate/upload & /rotate/download'$(tput init)"
+
+  if [[ $isUploadSuccess -eq 1 ]]; then
+    echo -e "\n$(tput setaf 1)$(tput bold)✗ [Failed] the test of Endpoint /rotate/upload so [FAILED]$(tput init)"
+  else
+    hello=$(curl --raw --output rotated.pdf -X GET http://localhost:$PORT/rotate/download && cat rotated.pdf | head -n 5 | grep -be "%PDF.*" | wc -l) # return value is stored in hello
+
+    if [[ $hello -gt 0 ]]; then
+      counterEndpoints=$((counterEndpoints+1))
+      echo -e "\n$(tput setaf 2)$(tput bold)✓ [Passed] the test of Endpoint /rotate/download$(tput init)"
+    else
+      echo -e "\n$(tput setaf 1)$(tput bold)✗ [Failed] the test of Endpoint /rotate/download$(tput init)"
+    fi
+  fi
+}
+
+
+AllFrontendEndpoint
+MergerTestCases
+RotatorTestCases
+
 
 echo -e "\n----------------------------------------------------------------"
+
+
 
 if [[ $counterEndpoints != $NO_OF_ENDPOINTS ]]; then
   echo -e "\n$(tput setaf 1)$(tput bold) ✗ $(($NO_OF_ENDPOINTS - $counterEndpoints))/$NO_OF_ENDPOINTS [FAILED] Tests$(tput init)"
