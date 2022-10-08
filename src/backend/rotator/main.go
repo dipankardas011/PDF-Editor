d78aae082f8c132d3f65b5995077e126c3f5cc6c
Dipankar Das <dipsonu10@hotmail.com>
2022-10-08 19:28:52 +0530

    [Observability](K8s)(Grafana)Addition of Observability for Rotate
    - [+] Added prom operator in backend/rotate
    - [+] new grafana dashboard
    - [+] service monitors
    
    Signed-off-by: Dipankar Das <dipsonu10@hotmail.com>

--- src/backend/rotator/main.go
+++ src/backend/rotator/main.go
@@ -5,7 +5,26 @@ import (
 	"net/http"
 	"os"
 	"time"
+
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
+
+var requestsProcessed = promauto.NewCounter(prometheus.CounterOpts{
+	Name: "go_request_operations_rotator_total",
+	Help: "The total number of processed requests",
+})
+
+var requestsProcessedError = promauto.NewCounter(prometheus.CounterOpts{
+	Name: "go_request_operations_rotator_error_total",
+	Help: "The total number of HTTP requests Errors",
+})
+
+var requestsProcessedSuccess = promauto.NewCounter(prometheus.CounterOpts{
+	Name: "go_request_operations_rotator_success_total",
+	Help: "The total number of HTTP 200 requests",
+})
 
 func getPort() string {
 	port := os.Getenv("PORT")
@@ -18,13 +37,19 @@ func getPort() string {
 }
 
 func greet(w http.ResponseWriter, r *http.Request) {
+	requestsProcessed.Inc()
 	fmt.Fprintf(w, "[ %s ] Hello from PDF-Rotator", time.Now())
+	requestsProcessedSuccess.Inc()
 }
 
 func DownloadFile(w http.ResponseWriter, r *http.Request) {
+	requestsProcessed.Inc()
 	if r.Method == "GET" {
+		requestsProcessedSuccess.Inc()
 		fmt.Println("{\"Source\": \"pdf-rotator\", \"operation\": \"Rotator\", \"Status\": \"Sending RotatedPDF\"}")
 		http.ServeFile(w, r, "uploads/resrelt.pdf")
+	} else {
+		requestsProcessedError.Inc()
 	}
 }
 
@@ -37,4 +62,5 @@ func main() {
 	http.HandleFunc("/upload", uploadHandler)
 	http.HandleFunc("/downloads", DownloadFile)
 	http.ListenAndServe(getPort(), nil)
+	http.Handle("/metrics", promhttp.Handler())
 }
